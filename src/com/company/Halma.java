d2af00fc3e224bba0a4f70fb73b4a2c606e0ac1b
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2022-11-04 12:47:30 +0330

    All unused codes and AI codes are removed

--- src/com/company/Halma.java
+++ src/com/company/Halma.java
@@ -3,33 +3,22 @@ package com.company;
 import java.awt.event.*;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 
 public class Halma {
 
 
-    private Icon empty = new ImageIcon("empty");
-
-    private Icon firstSelectionIcon, secondSelectionIcon;
-    private int firstX, firstY, secondX, secondY, prevFirstX, prevFirstY;
+    private Icon firstSelectionIcon;
+    private int firstX, firstY, secondX, secondY;
 
     private int firstSelectionLen;
     private boolean firstClick = true;
     private String firstSelectionStr;
 
-    private final static int maxDepth = 3;
     private Board board;
-    private JButton jbEndTurn;
-    private int playerTurn = 1;
-    private int moveCount = 0;
-    private int totalMoves = 0;
+    private int playerTurn;
 
-
-    private int grandTotalMoves = 0;
-
-    private Tile[][] tiles;
+    private final Tile[][] tiles;
 
     GUI gameUI = new GUI();
 
@@ -62,219 +51,63 @@ public class Halma {
         jk.CreateBoard();
         jk.CreateTextBoxArea();
         gameUI = jk;
-
         setUpGame();
-        turnButton();
 
-        // main layout
         jk.setTitle("Halma");
         jk.setVisible(true);
         jk.pack();
         jk.setSize(648, 800);
-        jk.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes frame
+        jk.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jk.setLocationRelativeTo(null);
-        jk.setVisible(true); // makes HalmaBoard visible
-        startGame();
+        jk.setVisible(true);
     }
 
     public void setUpGame() {
 
         gameUI.SetCampColors();
         gameUI.AddMarbles();
-//        givePieceMoves();
+        givePieceMoves();
         gameUI.AddFrame();
     }
-
-    private void startGame() {
-        if (CheckTerminal(tiles))
-            return;
-
-        if (playerTurn == 1)
-            doRandomAction(playerTurn);
-        else {
-            movePiece(doMinMax());
-        }
-
-        //doRandomAction(playerTurn);
-
-        try {
-            TimeUnit.MILLISECONDS.sleep(10);
-        } catch (Exception e) {
-
-        }
-        startGame();
-    }
-
-    private Move doMinMax() {
-        Pair temp = max(tiles, playerTurn, 0);
-        totalMoves++;
-        gameUI.PrintText("\n+ Value: " + temp.value + "    totalMoves: " + totalMoves+ "\n");
-        return temp.move;
-    }
-
-    private Pair max(Tile[][] currentBoard, int currentColor, int depth) {
-
-        if (CheckTerminal(currentBoard))
-            return new Pair(null, Integer.MIN_VALUE);
-
-        if (depth == maxDepth)
-            return new Pair(null, evaluate(currentBoard, currentColor));
-
-        List<Move> possibleMoves = createPossibleMoves(currentBoard, currentColor);
-        Move bestMove = null;
-        int bestMoveValue = Integer.MIN_VALUE;
-        for (Move move : possibleMoves) {
-            Pair temp = min(board.doMove(move, currentBoard), 3 - currentColor, depth + 1);
-            if (temp.value > bestMoveValue) {
-                bestMoveValue = temp.value;
-                bestMove = move;
-            }
-        }
-
-        return new Pair(bestMove, bestMoveValue);
-    }
-
-    private Pair min(Tile[][] currentBoard, int currentColor, int depth) {
-
-        if (CheckTerminal(currentBoard))
-            return new Pair(null, Integer.MAX_VALUE);
-
-        if (depth == maxDepth)
-            return new Pair(null, evaluate(currentBoard, currentColor));
-
-        List<Move> possibleMoves = createPossibleMoves(currentBoard, currentColor);
-        Move bestMove = null;
-
-        int bestMoveValue = Integer.MAX_VALUE;
-        for (Move move : possibleMoves) {
-            Pair temp = max(board.doMove(move, currentBoard), 3 - currentColor, depth + 1);
-            if (temp.value < bestMoveValue) {
-                bestMoveValue = temp.value;
-                bestMove = move;
-            }
-        }
-        return new Pair(bestMove, bestMoveValue);
-    }
-
-
-    private int evaluate(Tile[][] currentBoard, int currentColor) {
-        int score = 0;
-        for (int i = 0; i < currentBoard.length; i++) {
-            for (int j = 0; j < currentBoard.length; j++) {
-                if (currentBoard[i][j].color == playerTurn) {
-
-                    score += (7 - i);
-                    score += (7 - j);
-
-
-                } else if (currentBoard[i][j].color == (3 - playerTurn)) {
-
-                    score -= i;
-                    score -= j;
-
-                }
-            }
-        }
-        return score;
-    }
-
-
-    private void doRandomAction(int playerTurn) {
-
-        var possibleMoves = createPossibleMoves(tiles, playerTurn);
-        var random = new Random().nextInt(possibleMoves.size() - 1);
-        firstX = possibleMoves.get(random).startPos.x;
-        firstY = possibleMoves.get(random).startPos.y;
-        secondX = possibleMoves.get(random).finalPos.x;
-        secondY = possibleMoves.get(random).finalPos.y;
-        movePiece(possibleMoves.get(random));
-    }
-
-    private List<Move> createPossibleMoves(Tile[][] newBoard, int currentColor) {
-        List<Move> possibleMoves = new LinkedList<>();
-        for (int i = 0; i < 8; i++)
-            for (int j = 0; j < 8; j++)
-                if (tiles[i][j].color == currentColor) {
-                    firstX = i;
-                    firstY = j;
-                    List<Tile> legalTiles = new LinkedList<>();
-                    board.findPossibleMoves(newBoard, newBoard[firstX][firstY], legalTiles, newBoard[firstX][firstY], true);
-                    for (Tile tile : legalTiles)
-                        possibleMoves.add(new Move(newBoard[i][j], tile));
-                }
-        return possibleMoves;
-    }
-
-    public void turnButton() {
-
-        JPanel buttonsPanel = new JPanel();
-        jbEndTurn = new JButton("End Turn");
-
-        jbEndTurn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                // method that changes the players turn
-                if (prevFirstX <= 4 && prevFirstY <= 4 && grandTotalMoves < 2) {
-                    changeTurn(1);
-                } else if (prevFirstX <= 4 && prevFirstY <= 7 && grandTotalMoves < 2) {
-                    changeTurn(0);
-                } else {
-                    changeTurn(playerTurn);
-                }
-
-            }
-        });
-        buttonsPanel.add(jbEndTurn);
-        gameUI.GetJpanel().add(buttonsPanel);
-
-    }
-
 
     public void givePieceMoves() {
 
         for (int x = 0; x < gameUI.GetSquares().length; x++) {
             for (int y = 0; y < gameUI.GetSquares().length; y++) {
 
-                // Add action listener to every piece in the board
-                gameUI.GetSquares()[x][y].addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent ae) {
-                        gameUI.SetCampColors();
-                        if (firstClick) {
 
-                            String selectionText = ((JButton) ae.getSource()).getText();
-                            firstSelectionIcon = ((JButton) ae.getSource()).getIcon();
-                            String[] arr = selectionText.split(",");
-                            firstX = Integer.parseInt(arr[0]);
-                            firstY = Integer.parseInt(arr[1]);
+                gameUI.GetSquares()[x][y].addActionListener(ae -> {
+                    gameUI.SetCampColors();
+                    if (firstClick) {
 
-                            firstSelectionStr = firstSelectionIcon.toString();
-                            firstSelectionLen = firstSelectionStr.length();
+                        String selectionText = ((JButton) ae.getSource()).getText();
+                        firstSelectionIcon = ((JButton) ae.getSource()).getIcon();
+                        String[] arr = selectionText.split(",");
+                        firstX = Integer.parseInt(arr[0]);
+                        firstY = Integer.parseInt(arr[1]);
 
-                            if (tiles[firstX][firstY].color == (playerTurn)) {
-                                gameUI.PrintText("You have selected %s at %d, %d\n", firstSelectionStr, firstSelectionLen, firstX, firstY);
-                                Tile chosenTile = tiles[firstX][firstY];
-                                gameUI.ShowPossibleMoves(board.findPossibleMoves(tiles, chosenTile, null, chosenTile, true));
-                                firstClick = false;
-                            } else {
-                                gameUI.PrintText("You have selected an empty spot.\n");
-                            }
+                        firstSelectionStr = firstSelectionIcon.toString();
+                        firstSelectionLen = firstSelectionStr.length();
 
-                        } else { // save information about second click
-                            String selectionText = ((JButton) ae.getSource()).getText();
-                            secondSelectionIcon = ((JButton) ae.getSource()).getIcon();
-                            String[] arr = selectionText.split(",");
-                            secondX = Integer.parseInt(arr[0]);
-                            secondY = Integer.parseInt(arr[1]);
-
-                            if (isMoveLegal()) {
-                                movePiece(new Move(tiles[firstX][firstY], tiles[secondX][secondY]));
-                                prevFirstX = firstX;
-                                prevFirstY = firstY;
-                                moveCount++;
-                                grandTotalMoves++;
-                                //todo: checks if there is a winner here
-                            }
-                            firstClick = true;
+                        if (tiles[firstX][firstY].color == (playerTurn)) {
+                            gameUI.PrintText("You have selected %s at %d, %d\n", firstSelectionStr, firstSelectionLen, firstX, firstY);
+                            Tile chosenTile = tiles[firstX][firstY];
+                            gameUI.ShowPossibleMoves(board.findPossibleMoves(tiles, chosenTile, null, chosenTile, true));
+                            firstClick = false;
+                        } else {
+                            gameUI.PrintText("You have selected an empty spot.\n");
                         }
+
+                    } else {
+                        String selectionText = ((JButton) ae.getSource()).getText();
+                        String[] arr = selectionText.split(",");
+                        secondX = Integer.parseInt(arr[0]);
+                        secondY = Integer.parseInt(arr[1]);
+
+                        if (isMoveLegal()) {
+                            movePiece(new Move(tiles[firstX][firstY], tiles[secondX][secondY]));
+                        }
+                        firstClick = true;
                     }
                 });
             }
@@ -286,18 +119,14 @@ public class Halma {
         board.findPossibleMoves(tiles, tiles[firstX][firstY], legalTiles, tiles[firstX][firstY], true);
         Tile targetTile = tiles[secondX][secondY];
 
-        if (legalTiles.contains(targetTile))
-            return true;
-        return false;
+        return legalTiles.contains(targetTile);
     }
 
     public void changeTurn(int player) {
         gameUI.PrintText("Player %d has ended their turn.\n", player, player);
-        moveCount = 0;
         playerTurn = 3 - player;
 
     }
-
 
     public void movePiece(Move move) {
         firstX = move.startPos.x;
@@ -310,50 +139,4 @@ public class Halma {
         gameUI.UpdateGUI(tiles);
     }
 
-    private boolean CheckTerminal(Tile[][] currentTiles) {
-
-        int redCounter = 0;
-        int blueCounter = 0;
-
-        for (int x = 0; x < 8; x++) {
-            for (int y = 0; y < 8; y++) {
-                if (currentTiles[x][y].zone == 1) {
-                    if (currentTiles[x][y].color == 2) {
-                        redCounter++;
-                        if (redCounter >= 10) {
-                            gameUI.PrintText("Player 2 has won");
-                            return true;
-                        }
-                    }
-                } else if (currentTiles[x][y].zone == 2) {
-                    if (currentTiles[x][y].color == 1) {
-                        blueCounter++;
-                        if (blueCounter >= 10) {
-                            gameUI.PrintText("Player 1 has won");
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean CheckTerminal(Tile[][] currentTiles, int color) {
-
-        int inOpponentCampCounter = 0;
-
-        for (int x = 0; x < 8; x++) {
-            for (int y = 0; y < 8; y++) {
-                if (currentTiles[x][y].zone == (3 - color)) {
-                    if (currentTiles[x][y].color == color) {
-                        inOpponentCampCounter++;
-                        if (inOpponentCampCounter >= 10)
-                            return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
 }
